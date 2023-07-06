3fe318f9695eedcb934ee4aaa9385e83cbf35bba
Soomint <ttnne1@gmail.com>
2023-07-06 21:43:48 +0900

    주석 추가
    주석 추가

--- spa-router/src/Router.tsx
+++ spa-router/src/Router.tsx
@@ -2,7 +2,7 @@ import { useState, createContext, useContext, useEffect } from "react";
 
 // Router Node Interface
 interface RouterProps {
-    path: string;
+    path: string;               // 라우팅 경로
 }
 
 // Router Context
@@ -18,6 +18,7 @@ const Router = ({ children }: { children: React.ReactNode }) => {
         const handlePopstate = () => {
             setPath(location.pathname);
         };
+        // history entry 등록
         addEventListener('popstate', handlePopstate);
     }, []);
 
@@ -42,6 +43,8 @@ const Route = ({ path, component }: RouteProps) => {
     return path === contextPath ? component : null;
 };
 
+/////////////////////////////////// Hooks ///////////////////////////////////
+
 // useRouter Hooks Interface
 interface UseRouterProps {
     push(url: string): void;    // url이동
@@ -52,6 +55,7 @@ const useRouter = () => {
     const router: UseRouterProps = {
         // push 요청 시, 기존에 저장된 path를 수정한다.
         push(url) {
+            // history entry 등록
             history.pushState(null, "", url);
             dispatchEvent(new PopStateEvent('popstate'));
         },
@@ -59,5 +63,7 @@ const useRouter = () => {
 
     return router;
 };
+
+/////////////////////////////////// Hooks ///////////////////////////////////
 
 export { Router, Route, useRouter };
